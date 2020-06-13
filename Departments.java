4e0825c78fbcaeb892a39be94ef62af656fe76c0
Caroline Rives <carolinerives@Carolines-MacBook-Pro.local>
2020-06-13 15:19:09 -0400

    Created EmployeeList, created getters and setters for Department and List

--- Departments.java
+++ Departments.java
@@ -1,9 +1,40 @@
 package ems_project;
-import java.util.LinkedList;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Departments {
 	
-	List<String> employeeList = new LinkedList<>();
+	private String departmentName;
+	private Manager deptManager;
+	
+	private List<Employee> employeeList = new ArrayList<Employee>();
+	
+	public String getDepartmentName() {
+		return departmentName;
+	}
+	
+	public void setDepartmentName(String departmentName) {
+		this.departmentName = departmentName;
+	}
+	
+	public Manager getDeptManager() {
+		return deptManager;
+	}
+	
+	public void setDeptManager(Manager deptManager) {
+		this.deptManager = deptManager;
+	}
+	
+	public List<Employee> getEmployeeList() {
+		return employeeList;
+	}
+	
+	public void setEmployeeList(List<Employee> employeeList) {
+		this.employeeList = employeeList;
+	}
+	
+	
+	
 
 	
 }
